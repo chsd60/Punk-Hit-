b5e146d99cb230ebb8b15b6db38f8d4fb4cfa20c
M.T <nome.utente.che.nessuno.ha@gmail.com>
2021-05-01 11:04:55 +0200

    Aggiunto prototipo di sistema di respawn

---
+++ Assets/Punk Hit/Scripts/CharacterDeath.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CharacterDeath : MonoBehaviour {
+
+    // Dovrebbe andare riscritto in futuro, per renderlo più "pulito". Caricare tutta la scena di nuovo ricarica anche BGM e tutto il resto.
+
+    // Respawn = Numero di scena corrente
+    public int currentScene;
+    //timeToDeath indica dopo quanto il giocatore rispawnerà
+    public float timeToRespawn;
+
+    void Start() {
+        currentScene = SceneManager.GetActiveScene().buildIndex;
+    }
+    // Se il giocatore tocca il nemico, parte la coroutine
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player")) {
+            StartCoroutine(DeadPlayer());
+            Debug.Log("Dentro");
+        }
+    }
+
+    // Disattiva il game object del giocatore, e dopo due secondi ricarica la scena.
+    // Probabilmente andrà cambiato.
+    IEnumerator DeadPlayer() {
+
+        //La riga seguente servirà per quando verrà migliorato il sistema di respawn in futuro
+        // GameObject.Find("Player").GetComponent<PlayerMovement>().Death();
+        yield return new WaitForSeconds(timeToRespawn);
+        SceneManager.LoadScene(currentScene);
+    }
+}
