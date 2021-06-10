cf0606c4342ef1bbaf981ca6c1f1de025e00b771
M.T <nome.utente.che.nessuno.ha@gmail.com>
2021-06-10 11:56:05 +0200

    Aggiunto support visual scripting

--- Assets/Punk Hit/Scripts/Interagibili/ObjectAoE.cs
+++ Assets/Punk Hit/Scripts/Interagibili/ObjectAoE.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.VisualScripting;
 
 [RequireComponent(typeof(Collider))]
 public class ObjectAoE : MonoBehaviour {
@@ -12,7 +13,7 @@ public class ObjectAoE : MonoBehaviour {
     }
 
     private void OnTriggerEnter(Collider other) {
-        
+        CustomEvent.Trigger(other.gameObject, "Stun");
     }
 
 }
