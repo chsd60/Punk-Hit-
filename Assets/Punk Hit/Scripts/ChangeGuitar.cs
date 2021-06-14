16bfb9cae7d4bd0971c903024b6fe8408f6f80c7
M.T <nome.utente.che.nessuno.ha@gmail.com>
2021-06-14 19:17:02 +0200

    WIP additions

---
+++ Assets/Punk Hit/Scripts/ChangeGuitar.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeGuitar : MonoBehaviour {
+
+    private void Update() {
+        if(Input.GetButtonDown("NextGuitar")) {
+            
+        }
+    }
+}
