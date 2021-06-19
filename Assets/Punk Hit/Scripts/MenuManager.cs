33b92ac6606a940cf0b41d14fcc2a176d03cb57d
M.T <nome.utente.che.nessuno.ha@gmail.com>
2021-06-20 00:46:53 +0200

    Migliorato pickup membro, aggiunto menu di pausa (WIP)

---
+++ Assets/Punk Hit/Scripts/MenuManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour {
+
+    public bool menuStop = false;
+    private bool chiudiMenu = true; //da mettere a posto, al momento quando premo il pulsante si attiva sia l'apertura che la chiusura del menu simultaneamente
+    public GameObject pausaUI;
+
+    void FixedUpdate()
+    {
+        if (Input.GetButtonDown("Menu") && !menuStop) {
+            chiudiMenu = false;
+            Invoke("AttivaRichiudiMenu", 0.1f);
+            OpenMenu();
+        }
+
+        if (Input.GetButtonDown("Menu") && menuStop && chiudiMenu) {
+            CloseMenu();
+        }
+    }
+
+    public void OpenMenu() {
+        Debug.Log("Hey");
+        Time.timeScale = 0;
+        menuStop = true;
+        pausaUI.SetActive(true);
+    }
+
+    public void CloseMenu() {
+        Debug.Log("Listen");
+        Time.timeScale = 1;
+        menuStop = false;
+        pausaUI.SetActive(false);
+    }
+
+    private void AttivaChiudiMenu() {
+        chiudiMenu = false;
+    }
+}
