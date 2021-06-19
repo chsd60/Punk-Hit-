3be20834aea966a5c159066031db8587d1d48cce
M.T <nome.utente.che.nessuno.ha@gmail.com>
2021-06-19 11:17:22 +0200

    WIP sui tipi differenti di onde sonore

--- Assets/Punk Hit/Scripts/Waves/WaveSystem.cs
+++ Assets/Punk Hit/Scripts/Waves/WaveSystem.cs
@@ -2,36 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WaveSystem : MonoBehaviour {
+public class WaveSystem : GenericWSystem {
 
-    public GameObject ondaSonora;
-    //public float pushSpeed;
-    public float waveSpeed;
-    public float fineOnda;
-
-    private void Start() {
-        GetComponent<Rigidbody>().AddForce(transform.forward * waveSpeed, ForceMode.Impulse);
-        StartCoroutine(DeleteThis());
-    }
-
-
-    // Probabilmente dovrò spostare tutto in script diversi da mettere ai singoli GameObject.
-
-    //TODO: Creare script diversi per ogni onda.
+    //Distrugge l'onda.
     private void OnCollisionEnter(Collision other) {
         if (other.gameObject.GetComponent<DestroyableBase>() != null) {
             other.gameObject.GetComponent<DestroyableBase>().DestroyItem();
         }
         Destroy(gameObject);
     }
-
-    protected virtual void InitialPush() {
+     protected override void InitialPush() {
         GetComponent<Rigidbody>().AddForce(transform.forward * waveSpeed, ForceMode.Impulse);
-        StartCoroutine(DeleteThis());
-    }
-
-    IEnumerator DeleteThis() {
-        yield return new WaitForSeconds(fineOnda);
-        Destroy(ondaSonora);
-    }
+        base.InitialPush();
+     }
 }
