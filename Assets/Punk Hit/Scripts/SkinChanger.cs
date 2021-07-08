34df7d5c51a07885d1d088b770ed95dbe95ba59d
Marco Secchi <loziosecchi@gmail.com>
2021-07-08 09:50:59 +0200

    Add SkinChanger.

--- Assets/Punk Hit/Scripts/SkinChanger.cs
+++ Assets/Punk Hit/Scripts/SkinChanger.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
 
 public class SkinChanger : MonoBehaviour
@@ -9,6 +10,9 @@ public class SkinChanger : MonoBehaviour
     public Sprite[] skinElements;
     public void ChangeSkin()
     {
-        
+        for (var i = 0; i < renderers.Length; i++)
+        {
+            renderers[i].sprite = skinElements[i];
+        }
     }
 }
